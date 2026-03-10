0926d6f746c51e0af27458ec9613e4da92b6e796
dominics4n <dominics4n@gmail.com>
2026-03-10 17:37:55 -0600

    Comentarios

--- AStar.py
+++ AStar.py
@@ -21,6 +21,7 @@ Mapa = [[1,1,0,1,2,1,0,0,1,1],
         [1,1,1,0,0,1,0,0,1,1],
         [1,1,1,1,3,1,1,1,2,1]]
 
+#Mapa que se imprime en la consola
 MapaVisual = [["Y / X",1,2,3,4,5,6,7,8,9,10]]
 y_guia = 0
 for fila in Mapa:
@@ -28,11 +29,13 @@ for fila in Mapa:
     filavisual = [y_guia]
     for columna in fila:
         if columna == 0:
+            #Cambia los 0 por X
             filavisual.append("X")
         else:
             filavisual.append(columna)
     MapaVisual.append(filavisual)
 
+#Revisar que las coordenadas del input esten dentro del mapa
 def xyValido(xyString):
     while True:
         try:
@@ -44,6 +47,7 @@ def xyValido(xyString):
         except ValueError:
             print("La coordenada " + xyString + " debe de ser un numero entero")
 
+#Revisa que las coordenadas del input no sean casillas bloqueadas
 def casilla_Valida(ini_fin):
     while True:
         print("Mapa: ")
@@ -59,64 +63,73 @@ def casilla_Valida(ini_fin):
 
 inicio = casilla_Valida("inicio")
 meta = casilla_Valida("meta")
+#Sw asegura que las casillas de inicio y meta sean diferentes
 while meta == inicio:
     print("La meta tiene que ser diferente a la casilla inicial")
     meta = casilla_Valida("meta")
 buscando = True
 celda_Actual = inicio
+#Casillas que se pueden visitar
 por_Visitar = []
 celda = []
+#Movimientos permitidos 
 movimientos = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    
+
+#Crea los objetos de clase celda para cada casilla del mapa
 for fila in Mapa:
     datos = []
     for columna in fila:
         datos.append(Celdas())
     celda.append(datos)
 
+#Calcula el score de cadaa casilla
 def aScore(X, Y):
     costo = celda[celda_Actual[0]][celda_Actual[1]].gn + Mapa[X][Y]
     euristica = abs(meta[0] - X) + abs(meta[1] - Y)
     return([costo, costo + euristica])
 
 celda[inicio[0]][inicio[1]].parentxy = inicio
+#Agrega la casilla inicial para comenzar la busqueda
 por_Visitar.append(inicio)
 
-vuelta = 0
 while buscando:
-    vuelta = vuelta + 1
     minFN = celda[celda_Actual[0]][celda_Actual[1]].fn
     indice = 0
     indice_menor = 0
+    #Busca la casilla con el score fn menor
     for coord in por_Visitar:
         if celda[coord[0]][coord[1]].fn < minFN:
             minFN = celda[coord[0]][coord[1]].fn
             indice_menor = indice
         indice = indice +1
+    #Saca la celda con el menor fn y la marca como visitada
     celda_Actual = por_Visitar.pop(indice_menor)
-
     celda[celda_Actual[0]][celda_Actual[1]].visitada = True
-
+    #Mueve las coordenadas segun los movimientos permitidos
     for mov in movimientos:
         newX = celda_Actual[0] + mov[0]
         newY = celda_Actual[1] + mov[1]
-        #Si coordenadas dentro de la cuadricula
+        #Revisa que las coordenadas esten dentro de la cuadricula
         if newX >= 0 and newX < 10 and newY >= 0 and newY < 10:
-            #Si celda no esta bloqueada o fue visitada
+            #Revisa que la celda por revisar no este bloqueada ni haya sido visidada
             if Mapa[newX][newY] > 0 and celda[newX][newY].visitada == False:
                 newXY = [newX, newY]
+                #Revisa si la celda es la meta
                 if newXY == meta:
                     celda[newX][newY].parentxy = celda_Actual
                     buscando = False
                 else:
+                    #Calcula su fn y gn
                     GN_FN = aScore(newX, newY)
                     GN = GN_FN[0]
                     FN = GN_FN[1]
+                    #Si el nuevo fn es menor al que ya se tenia, se guardan los nuevos datos
                     if celda[newX][newY].fn == -1 or FN < celda[newX][newY].fn:
                         celda[newX][newY].fn = FN
                         celda[newX][newY].gn = GN
                         celda[newX][newY].parentxy = celda_Actual
                         if newXY not in por_Visitar:
+                            #Se agrega la casilla a la lista de celdas por visitar
                             por_Visitar.append(newXY)
 
 print("El camino es:")
